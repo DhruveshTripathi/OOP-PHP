de20f6fc9a6f41a76be2b1c978474d526b6a0a0f
DhruveshTripathi <dhruvesh_tripathi@yahoo.com>
2015-12-11 15:16:33 +0530

    Folder added for all the programs in chapter 1 and 2

---
+++ Chapter - 1,2/emailer.php
@@ -0,0 +1,43 @@
+<?php
+
+class emailer
+{
+	private $sender;
+	private $recipients;
+	private $subject;
+	private $body;
+	function __construct($sender)
+	{
+		$this->sender = $sender;
+		$this->recipients = array();
+	}
+	public function addRecipients($recipient)
+	{
+		array_push($this->recipients, $recipient);
+	}
+	public function setSubject($subject)
+	{
+		$this->subject = $subject;
+	}
+	public function setBody($body)
+	{
+		$this->body = $body;
+	}
+	public function sendEmail()
+	{
+		foreach ($this->recipients as $recipient)
+		{
+			$result = mail($recipient, $this->subject, $this->body,"From: {$this->sender}\r\n");
+			if ($result) 
+				echo "Mail successfully sent to{$recipient}<br/>";
+		}
+	}
+}
+$emailer = new emailer("dhruvesh_tripathi@yahoo.com"); //construcion
+$emailer->addRecipients("dhruveshgr8@gmail.com"); //accessing methods
+// and passing some data
+$emailer->setSubject("Just a Test");
+$emailer->setBody("Hi Dhruvesh, How are you?");
+$emailer->sendEmail();
+
+?>
